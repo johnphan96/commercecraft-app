a1c97e2e90eb37700a3d7a01296ff37e3a6f0259
John Phan <johnphan010@gmail.com>
2021-12-28 12:32:38 -0800

    Connect backend to MongoDB using Mongoose and create users

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost/commercecraft",
+  {}
+);
 
 app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ message: "Product not found" });
+    res.status(404).send({ message: "Products not found" });
   }
 });
 
@@ -16,8 +22,14 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
+app.use("/api/users", userRouter);
+
 app.get("/", (req, res) => {
   res.send("Server is ready");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
