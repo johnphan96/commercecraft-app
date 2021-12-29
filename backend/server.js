bf879249edc6577eecd2def7ff2ff7c22455dc0a
John Phan <johnphan010@gmail.com>
2021-12-29 15:27:36 -0800

    Implement signin API using JWT Auth

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+// Add middle to parse json data in body of post request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect(
   process.env.MONGODB_URL || "mongodb://localhost/commercecraft",
   {}
