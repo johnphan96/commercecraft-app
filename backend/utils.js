587ac692b0f28b715825b0eba1dc49c539e341fd
John Phan <johnphan010@gmail.com>
2022-01-05 15:53:26 -0800

    Implement place order API

--- backend/utils.js
+++ backend/utils.js
@@ -14,3 +14,27 @@ export const generateToken = (user) => {
     }
   );
 };
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    // splice to get token part
+    const token = authorization.slice(7, authorization.length);
+
+    // decrypt token
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || "somethingsecret",
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: "Invalid Token" });
+        } else {
+          req.user = decode;
+          next();
+        }
+      }
+    );
+  } else {
+    res.status(401).send({ message: "No Token" });
+  }
+};
